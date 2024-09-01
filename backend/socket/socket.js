84058919e1cbdb33649a124b33f7e099f0b7a3bb
AnishXD <110338891+AnishXD@users.noreply.github.com>
2024-09-01 15:03:24 +0530

    Update socket.js yes

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-        origin:['http://localhost:3000'],
+        origin:['https://final-chat-frontend-six.vercel.app'],
         methods:['GET', 'POST'],
     },
 });
